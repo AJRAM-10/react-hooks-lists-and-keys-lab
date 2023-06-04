9b15c962721f46b099b13d6fc60056a51e5d1642
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-06-03 21:22:10 -0400

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const pageLink = links.map((link) => (
+    <a key={link} href={`#${link}`}>{link}</a>
+  ))
+
+  return <nav>{pageLink}</nav>;
 }
 
 export default NavBar;
